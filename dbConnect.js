10e58512f652d73264cd188be44f3625431850f8
Yash G <98229201+yashgoyalg@users.noreply.github.com>
2023-02-01 11:13:04 +0530

    Add files via upload

---
+++ dbConnect.js
@@ -0,0 +1,22 @@
+//database cannection 
+
+const mongoose = require("mongoose");    //require
+
+const dbConnect = () => {
+    const connectionParams = { useNewUrlParser: true };
+    mongoose.connect(process.env.DB, connectionParams); //by this we are connect with database
+
+    mongoose.connection.on("connected", () => {
+		console.log("Connected to database sucessfully");
+	});
+
+	mongoose.connection.on("error", (err) => {
+		console.log("Error while connecting to database :" + err);
+	});
+
+	mongoose.connection.on("disconnected", () => {
+		console.log("Mongodb connection disconnected");
+	});
+};
+
+module.exports = dbConnect;  //we are exporting 
